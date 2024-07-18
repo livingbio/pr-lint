34f88fcd5c413d55862b439e770b2552440ae40f
lucemia <lucemia@gmail.com>
2024-07-18 13:30:52 +0800

    add lint rule, PR title should include reference while hotfix

--- src/pr_lint/tests/test_utils.py
+++ src/pr_lint/tests/test_utils.py
@@ -1,4 +1,4 @@
-from ..utils import collect_emojis_from_labels, extract_owner_from_title, extract_pure_title
+from ..utils import collect_emojis_from_labels, extract_owner_from_title, extract_pure_title, extract_reference_number
 
 
 def test_extract_owner_from_title() -> None:
@@ -7,6 +7,14 @@ def test_extract_owner_from_title() -> None:
 
     title = "This is a PR title"
     assert extract_owner_from_title(title) is None
+
+
+def test_extract_reference_number() -> None:
+    title = "This is a PR title (#123)"
+    assert extract_reference_number(title) == "123"
+
+    title = "This is a PR title #123"
+    assert extract_reference_number(title) is None
 
 
 def test_extract_pure_title() -> None:
